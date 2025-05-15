33e261ab9be8d7693b3745e74fd36a5e545a17c5
David Ochiel <nguonodave@gmail.com>
2025-05-15 17:09:15 +0300

    feat: add sessions table

--- schema/schema.sql
+++ schema/schema.sql
@@ -61,3 +61,12 @@ CREATE TABLE IF NOT EXISTS reactions (
     ),
     UNIQUE(user_id, post_id, comment_id)
 );
+
+-- Sessions
+CREATE TABLE IF NOT EXISTS sessions (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    expires_at DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
