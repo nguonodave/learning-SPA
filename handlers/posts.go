7f3a9187755c6b9c96a803b2f0af1c6034be01a4
David Ochiel <nguonodave@gmail.com>
2025-05-21 13:10:01 +0300

    fix(error logs): match logs for reactionsErr and categErrs

--- handlers/posts.go
+++ handlers/posts.go
@@ -273,13 +273,13 @@ func ListPostsHandler(db *sql.DB) http.HandlerFunc {
 
 			categories, categErrs := GetPostCategories(db, post.ID)
 			if categErrs != nil {
-				log.Println("error getting categories from db", err)
+				log.Println("error getting categories from db", categErrs)
 				http.Error(w, "An error occured, kindly check back later", http.StatusInternalServerError)
 			}
 			post.Categories = categories
 
 			likesCount, dislikesCount, reactionsErr := GetReactionCountsForPost(db, post.ID)
-			if categErrs != nil {
+			if reactionsErr != nil {
 				log.Println("error getting reactions count from db", reactionsErr)
 				http.Error(w, "An error occured, kindly check back later", http.StatusInternalServerError)
 			}
